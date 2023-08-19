29f73826017011d831b193f98f39cc396b076f63
ohmycloud <ohmycloudy@gmail.com>
2023-08-19 16:47:24 +0800

    add comment

--- src/main/scala/SchedulingMeetings.scala
+++ src/main/scala/SchedulingMeetings.scala
@@ -33,16 +33,42 @@ object SchedulingMeetings extends App {
   }
 
   // IO[A] is a value that represents a potentially side-effect IO action
-
+  // This function returns a description of a side-effectful IO action that,
+  // when executed, will return a list of MeetingTimes.
   def calendarEntries(name: String): IO[List[MeetingTime]] = {
     IO.delay(calendarEntriesApiCall(name))
   }
 
+  // This function returns a description of a side-effectful IO action that,
+  // when executed, will not return anything(hence IO[Unit]).
   def createMeeting(names: List[String],
                     meeting: MeetingTime
                    ): IO[Unit] = {
     IO.delay(createMeetingApiCall(List("Alice", "Bob"), MeetingTime(11, 12)))
   }
+
+  // check whether two meeting times overlap.
+  def meetingsOverlap(meeting1: MeetingTime, meeting2: MeetingTime): Boolean = {
+    meeting1.endHour > meeting2.startHour &&
+    meeting2.endHour > meeting1.startHour
+  }
+
+  def possibleMeetings(
+    existingMeetings: List[MeetingTime],
+    startHour: Int,
+    endHour: Int,
+    lengthHours: Int
+  ): List[MeetingTime] = {
+    val slots = List
+      .range(startHour, endHour - lengthHours + 1)
+      .map(startHour => MeetingTime(startHour, startHour + lengthHours))
+    slots.filter(slot =>
+      existingMeetings.forall(meeting => !meetingsOverlap(meeting, slot))
+    )
+  }
+
+  // This function returns a description of a side-effectful IO action that,
+  // when executed, will return a list of MeetingTimes for two attendees.
   def scheduledMeetings(person1: String, person2: String): IO[List[MeetingTime]] = {
     for {
       meetings1 <- calendarEntries(person1)
@@ -50,5 +76,15 @@ object SchedulingMeetings extends App {
     } yield meetings1.appendedAll(meetings2)
   }
 
-  scheduledMeetings("Alice", "Bob")
+  // This function returns a description of a side-effectful IO action that, when executed,
+  // will return a possible MeetingTime. It is build using smaller descriptions(IO values),
+  // but no action is executed in this function. It's all just a combination of descriptions.
+  def schedule(person1: String, person2: String, lengthHours: Int): IO[Option[MeetingTime]] = {
+    for {
+      existingMeetings <- scheduledMeetings(person1, person2)
+      meetings = possibleMeetings(existingMeetings, 8, 16, lengthHours)
+    } yield meetings.headOption
+  }
+
+  println(schedule("Alice", "Bob", 2).unsafeRunSync())
 }
